a097a3395cf3df78e4f61b73751d23921c02b137
NikitaPopovA <gantss12@mail.ru>
2023-07-28 18:08:53 +0300

    ПР13-1

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,10 @@
-const User = require("../models/user");
+const User = require('../models/user');
 const {
   CREATED_CODE,
   BAD_REQUEST_CODE,
   NOT_FOUND_CODE,
   statusDefaultError,
-} = require("../utils/errorMessages");
+} = require('../utils/errorMessages');
 
 const getUsers = (req, res) => {
   User.find({})
@@ -21,9 +21,9 @@ const createUser = (req, res) => {
   })
     .then((user) => res.status(CREATED_CODE).send(user))
     .catch((err) => {
-      if (err.name === "ValidationError") {
+      if (err.name === 'ValidationError') {
         res.status(BAD_REQUEST_CODE).send({
-          message: "Даннные не коректны в создании пользователя.",
+          message: 'Даннные не коректны в создании пользователя.',
         });
         return;
       }
@@ -34,17 +34,17 @@ const createUser = (req, res) => {
 const getUser = (req, res) => {
   User.findById(req.params.userId)
     .orFail(() => {
-      throw new Error("NotFoundError");
+      throw new Error('NotFoundError');
     })
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.message === "NotFoundError") {
+      if (err.message === 'NotFoundError') {
         res
           .status(NOT_FOUND_CODE)
-          .send({ message: "Пользователь по указанному id не найден." });
-      } else if (err.name === "CastError") {
+          .send({ message: 'Пользователь по указанному id не найден.' });
+      } else if (err.name === 'CastError') {
         res.status(BAD_REQUEST_CODE).send({
-          message: "Переданы некорректные данные.",
+          message: 'Переданы некорректные данные.',
         });
       } else {
         statusDefaultError(err, res);
@@ -60,21 +60,21 @@ const updateUser = (req, res) => {
     { new: true, runValidators: true }
   )
     .orFail(() => {
-      throw new Error("NotFoundError");
+      throw new Error('NotFoundError');
     })
     .then((updateData) => {
       res.send(updateData);
     })
     .catch((err) => {
-      if (err.message === "NotFoundError") {
+      if (err.message === 'NotFoundError') {
         res.status(NOT_FOUND_CODE).send({
-          message: "Пользователь по указанному _id не найден.",
+          message: 'Пользователь по указанному _id не найден.',
         });
         return;
       }
-      if (err.name === "ValidationError") {
+      if (err.name === 'ValidationError') {
         res.status(BAD_REQUEST_CODE).send({
-          message: "Переданы некорректные данные при обновлении профиля.",
+          message: 'Переданы некорректные данные при обновлении профиля.',
         });
         return;
       }
